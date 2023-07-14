0c3c757c344028b53ec76017cbf79d2b200f83ef
Kendall <kendallawills@gmail.com>
2023-07-14 10:48:51 -0400

    Built In Objects

--- app.js
+++ app.js
@@ -22,3 +22,8 @@ console.log('Teaching the world how to code'.length);
 console.log('Codecademy'.toUpperCase());
 console.log('    Remove whitespace    '.trim());
 
+// Built In Objects
+console.log(Math.random() * 100);
+console.log(Math.floor(Math.random() * 100));
+console.log(Math.ceil(43.8));
+console.log(Number.isInteger(2017));
