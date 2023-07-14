017acb1c197d471a68668a4d21e65aecebaa7429
Kendall <kendallawills@gmail.com>
2023-07-14 10:24:24 -0400

    Add Arithmetic

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
+// Words, Strings, and Numbers
 console.log(34);
 console.log('JavaScript');
 console.log('Woohoo! I love to code! #codecademy');
+
+// Arithmetic Operators
+console.log(3.5 + 34);
+console.log(2023 - 1969);
+console.log(65 / 240);
+console.log(0.2708 * 100);
+console.log(11 % 3);
